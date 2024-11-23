e7e5c83f0f3b5cfc4c39c0b63f86114b42249a47
Rosiane Cristina Souza dos Passos <82117815+rosianepassos@users.noreply.github.com>
2024-11-23 15:46:59 -0300

    Add files via upload

---
+++ CadastrarVenda.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="CadastrarVenda.aspx.cs" Inherits="CadastrarVenda" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        #Text1
+        {
+            width: 144px;
+        }
+        #BtnSave
+        {
+            width: 72px;
+        }
+        #Text2
+        {
+            width: 202px;
+        }
+        #Text3
+        {
+            width: 179px;
+        }
+        #Select1
+        {
+            width: 143px;
+            height: 26px;
+        }
+        #Select2
+        {
+            height: 21px;
+            width: 138px;
+        }
+        #Select3
+        {
+            height: 16px;
+            width: 127px;
+        }
+        #BtnBuscar2
+        {
+            height: 33px;
+            width: 77px;
+        }
+        #BtnBuscar
+        {
+            height: 33px;
+            width: 70px;
+        }
+        #SelectFuncionario
+        {
+            height: 33px;
+            width: 131px;
+        }
+        #SelectCliente
+        {
+            height: 12px;
+            width: 132px;
+        }
+        #SelectItemVenda
+        {
+            height: 22px;
+            width: 144px;
+        }
+        .style2
+        {
+            width: 312px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table style="width:100%;">
+        <tr>
+            <td class="style2">
+                Data Venda:<asp:TextBox 
+                    ID="TxtDataVenda" runat="server" Height="23px" 
+                    Width="164px"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+                    ControlToValidate="TxtDataVenda" ErrorMessage="Campo Obrigatório"></asp:RequiredFieldValidator>
+            </td>
+            <td>
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td class="style2">
+                Numero Nota Fiscal:<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            </td>
+            <td>
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td class="style2">
+                &nbsp;</td>
+            <td>
+                <asp:Button ID="BtnSave" runat="server" Text="Salvar" />
+                <asp:Button ID="BtnCancelar" runat="server" Text="Cancelar" />
+            </td>
+            <td>
+                &nbsp;</td>
+        </tr>
+    </table>
+    </asp:Content>
+
---
+++ Venda.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class Venda
+    {
+        private String ConStr = @"Data Source=.\SQLEXPRESS;Initial Catalog=bancovendas;Integrated Security=True";
+
+        public DTO.Vendas Select()
+        {
+            DTO.Vendas vendas = new DTO.Vendas();
+            SqlConnection con = new SqlConnection(ConStr);
+            string sql = "SelecionaVenda";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                DTO.Venda v = new DTO.Venda();
+
+                v.DataVenda = dr.GetDateTime(0);
+                v.IdPessoa = dr.GetInt32(1);
+                v.NumeroNotaFiscal = dr.GetInt32(2);
+                vendas.Add(v);
+
+
+
+
+            }
+
+            con.Close();
+
+            return vendas;
+        }
+
+        public void Update(DTO.Venda v)
+        {
+            SqlConnection con = new SqlConnection(ConStr);
+            string sql = "AtualizaVenda";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@DataVenda", v.DataVenda);
+            cmd.Parameters.AddWithValue("@IdPessoa", v.IdPessoa);
+            cmd.Parameters.AddWithValue("@NumNotaFiscal", v.NumeroNotaFiscal);
+
+
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void Insert(DTO.Venda v)
+        {
+            SqlConnection con = new SqlConnection(ConStr);
+            string sql = "InsereVenda";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@DataVenda", v.DataVenda);
+            cmd.Parameters.AddWithValue("@IdPessoa", v.IdPessoa);
+            cmd.Parameters.AddWithValue("@NumNotaFiscal", v.NumeroNotaFiscal);
+
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void Delete(DTO.Venda v)
+        {
+            SqlConnection con = new SqlConnection(ConStr);
+            string sql = "DeleteVenda";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", v.IdVenda);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
